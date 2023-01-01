0aa4790a19a6b8fbe5038670f0b234d6748e4ac0
Chuang Zhu <git@chuang.cz>
2023-01-01 18:01:45 +0800

    ~

--- bilibili-chat.lua
+++ bilibili-chat.lua
@@ -5,7 +5,7 @@ local zlib = require'zlib'
 function to_bytes(int)
 	local t = {}
 	for i = 0, 3 do
-    	t[4-i] = int % 0x100
+		t[4-i] = int % 0x100
 		int = math.floor(int / 0x100)
 	end
 	return string.char(table.unpack(t))
@@ -19,19 +19,10 @@ function from_bytes(str)
 	return int
 end
 
--- function encode(op, payload)
--- 	return to_bytes(16 + #payload) .. '\0\16\0\1' .. to_bytes(op) .. '\0\0\0\1' .. payload
--- end
 function send(client, op, payload)
 	client:send(to_bytes(16 + #payload) .. '\0\16\0\1' .. to_bytes(op) .. '\0\0\0\1' .. payload)
 end
 
--- function decode(str)
--- 	local length = from_bytes(string.sub(str, 1, 4))
--- 	local protover = from_bytes(string.sub(str, 7, 8))
--- 	local op = from_bytes(string.sub(str, 9, 12))
--- 	local payload = string.sub(str, 17, length)
--- end
 function receive(client)
 	local length = from_bytes(client:receive(4))
 	client:receive(2)
@@ -41,17 +32,22 @@ function receive(client)
 	local payload = client:receive(length - 16)
 	if protocol == 2 then
 		local inflate = zlib.inflate()
-		payload = string.sub(inflate(payload), 17)
+		-- FIXME: there may be multiple packs in the payload
+		local pack = inflate(payload)
+		length = from_bytes(string.sub(pack, 1, 4))
+		protocol = from_bytes(string.sub(pack, 7, 8))
+		op = from_bytes(string.sub(pack, 9, 12))
+		payload = string.sub(pack, 17, length)
 	end
-	return length, protocol, op, payload
+	return protocol, op, payload
 end
 
 local client = socket.tcp()
 client:connect('broadcastlv.chat.bilibili.com', 2243)
 send(client, 7, '{"roomid":23058}')
 while true do
-	local length, protocol, op, payload = receive(client)
-	if protocol == 0 or protocol == 2 then
+	local protocol, op, payload = receive(client)
+	if protocol == 0 and op == 5 then
 	end
 end
 send(client, 2, '')
